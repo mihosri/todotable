3f722a00c4a51e9a68d1f74bf9de4a8e6047533e
mihosri <113211399+mihosri@users.noreply.github.com>
2024-02-24 14:32:03 -0700

    Added Documentation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import { Component } from "react";
 import Todo from "./todo";
 
-class App extends Component
-{
+class App extends Component {
+  /**
+   *Stored task values in the state so the app will respond based on the state updates
+   *
+   * @memberof App
+   */
   state = {
-    tasks: ["study","cook","work","sing","laundry","clean"]
+    tasks: ['study', 'cook', 'work', 'sing', 'laundry', 'clean'],
   }
 
-  deleteTask = (index) => 
-  {
-    const {tasks} = this.state;
+  /**
+   *Function to remove the task value and update the state with the rest
+   *Utilized Filter method, updated the state with the filtered value
+   * @param {*} index - removing index value
+   * @memberof App
+   */
+  deleteTask = (index) => {
+    const { tasks } = this.state
 
     let filteredData = tasks.filter((task, i) => {
-      return i!== index;
+      return i !== index
     })
 
-    this.setState({tasks: filteredData});
+    this.setState({ tasks: filteredData })
   }
-
-  render(){
-    const {tasks} = this.state
+/**
+ *App is the parent component to Todo
+ *Attaching the state data and deleteTask as props to Todo
+ * @return {*} 
+ * @memberof App
+ */
+render() {
+    const { tasks } = this.state
     return (
       <div>
         <Todo deleteTaskFunc={this.deleteTask} taskData={tasks} />
       </div>
     )
   }
-  
 }
 
 export default App;
--- src/todo.jsx
+++ src/todo.jsx
@@ -1,5 +1,9 @@
 const { Component } = require('react')
-
+/**
+ *Heading for the table content
+ *
+ * @return {*} 
+ */
 const TableHead = () => {
   return (
     <thead>
@@ -10,7 +14,12 @@ const TableHead = () => {
     </thead>
   )
 }
-
+/**
+ *Display the content from state; Utilized map method
+ *state data and deleteTask received as props
+ * @param {*} props
+ * @return {*} 
+ */
 const TableBody = (props) => {
   const { taskData, deleteTaskFunc } = props
   const rows = taskData.map((task, index) => {
@@ -28,7 +37,12 @@ const TableBody = (props) => {
 
   return (<tbody>{rows}</tbody>)
 }
-
+/**
+ *Todo is a class component that comprises the TableHead and TableBody functional components
+ *
+ * @class Todo
+ * @extends {Component}
+ */
 class Todo extends Component {
   render() {
     const { deleteTaskFunc, taskData } = this.props
