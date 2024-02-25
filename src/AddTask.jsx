259d69dbad15959a69990f5c872e81bd6184cf84
mihosri <113211399+mihosri@users.noreply.github.com>
2024-02-25 00:26:58 -0700

    Added doc

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
 
 export class AddTask extends Component {
+
+  /**
+   *This is used to set the state of form back after submitting the data
+   *
+   * @memberof AddTask
+   */
   initialState = {
     task:""
   }
-
-  state = this.initialState
-
-  handleChange = (event) => {
+/**
+ *Current state value
+ *
+ * @memberof AddTask
+ */
+state = this.initialState
+/**
+ *When the user gives input, correspondingly attach it to the state
+ *
+ * @param {*} event
+ * @memberof AddTask
+ */
+handleChange = (event) => {
     const {name, value} = event.target;
    this.setState({[name]:value}) 
   }
-
-  clickSubmit = () => {
+/**
+ *This event handler uses the handleSubmit method from App component through props
+ *
+ * @memberof AddTask
+ */
+clickSubmit = () => {
     let task = this.state
     this.props.handleSubmit(task)
     this.setState(this.initialState)
