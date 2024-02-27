79e17ab658655d6fc9052c2253632f3999ef48b2
mihosri <113211399+mihosri@users.noreply.github.com>
2024-02-26 17:44:11 -0700

    store action

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {store} from './store';
 
+store.dispatch({
+  type: 'taskAdded',
+  payload: {
+    task: 'task1',
+  },
+})
 
+store.dispatch({
+  type: 'taskCompleted',
+  payload: {
+    id: '0',
+  },
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/store.js
+++ src/store.js
@@ -3,18 +3,6 @@ import {reducer} from './reducer';
 
 const store = createStore(reducer);
 
-store.dispatch({
-    type: "taskAdded",
-    payload: {
-        task: "task1"
-    }
-})
 
-store.dispatch({
-  type: 'taskCompleted',
-  payload: {
-    id: '0',
-  },
-})
 
 export default store;
