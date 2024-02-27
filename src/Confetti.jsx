2169470be28b83fb8d1118c378ea39b86ad1e8e4
mihosri <113211399+mihosri@users.noreply.github.com>
2024-02-26 17:24:28 -0700

    Confetti component created

---
+++ src/Confetti.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Confetti extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Confetti
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <>
+        <h1>Todo Ship</h1>
+        <p>Get on and reach your destination without load!</p>
+      </>
+    )
+  }
+}
+
+export default Header
