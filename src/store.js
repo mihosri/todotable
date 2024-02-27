d48cd2264f3c3fd3b7959030ddd0acfd4e552670
mihosri <113211399+mihosri@users.noreply.github.com>
2024-02-26 17:42:34 -0700

    store actions and dispatch

--- src/store.js
+++ src/store.js
@@ -3,4 +3,18 @@ import {reducer} from './reducer';
 
 const store = createStore(reducer);
 
+store.dispatch({
+    type: "taskAdded",
+    payload: {
+        task: "task1"
+    }
+})
+
+store.dispatch({
+  type: 'taskCompleted',
+  payload: {
+    id: '0',
+  },
+})
+
 export default store;
